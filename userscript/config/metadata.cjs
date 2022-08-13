07c28717ddd36ae634ead15110bf069bc8ac99b0
Rendy Arya Kemal <renrror@gmail.com>
2022-08-13 15:03:56 +0700

    Reformat + only use local on dev

--- userscript/config/metadata.cjs
+++ userscript/config/metadata.cjs
@@ -1,30 +1,24 @@
-const { author, dependencies, repository, version } = require('../package.json')
+const {
+  author,
+  dependencies,
+  repository,
+  version,
+} = require("../package.json");
 
 module.exports = {
-  name: 'osu! Discussions Vote',
-  namespace: 'https://rorre.xyz/',
+  name: "osu! Discussions Vote",
+  namespace: "https://rorre.xyz/",
   version: version,
   author: author,
   source: repository.url,
   // 'license': 'MIT',
-  match: [
-    'https://osu.ppy.sh/*',
-    'http://127.0.0.1:8080/*'
-  ],
+  match: ["https://osu.ppy.sh/*"],
   require: [
     `https://cdn.jsdelivr.net/npm/jquery@${dependencies.jquery}/dist/jquery.min.js`,
     `https://cdn.jsdelivr.net/npm/axios@${dependencies.axios}/dist/axios.min.js`,
-    `https://cdn.jsdelivr.net/npm/axios-userscript-adapter@${dependencies['axios-userscript-adapter']}/dist/axiosGmxhrAdapter.min.js`,
+    `https://cdn.jsdelivr.net/npm/axios-userscript-adapter@${dependencies["axios-userscript-adapter"]}/dist/axiosGmxhrAdapter.min.js`,
   ],
-  grant: [
-    'GM.xmlHttpRequest',
-    'GM.setValue',
-    'GM.getValue'
-  ],
-  connect: [
-    'httpbin.org',
-    '127.0.0.1',
-    '127.0.0.1:5000'
-  ],
-  'run-at': 'document-end'
-}
+  grant: ["GM.xmlHttpRequest", "GM.setValue", "GM.getValue"],
+  connect: ["httpbin.org", "127.0.0.1", "127.0.0.1:5000"],
+  "run-at": "document-end",
+};
